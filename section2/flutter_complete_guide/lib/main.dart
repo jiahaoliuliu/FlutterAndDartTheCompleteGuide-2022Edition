da67510dcb66b97efd55f1b73df6404428c46779
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-04-03 15:29:13 +0400

    Set up the basic layout to one question and 3 buttons

--- section2/flutter_complete_guide/lib/main.dart
+++ section2/flutter_complete_guide/lib/main.dart
@@ -3,14 +3,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }

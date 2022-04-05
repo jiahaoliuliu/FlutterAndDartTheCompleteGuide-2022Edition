97363143465f663ae8f874af2af1b0e9e199b5ad
Jiahao Liu <jiahao.liu@talabat.com>
2022-04-05 15:01:26 +0400

    JL Set up teh list of responds on the questions map as answers

--- section2/flutter_complete_guide/lib/main.dart
+++ section2/flutter_complete_guide/lib/main.dart
@@ -48,9 +48,10 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(_questions[_questionIndex]['questionText']),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            ...(_questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),

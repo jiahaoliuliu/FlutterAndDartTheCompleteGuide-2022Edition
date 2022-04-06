672f19bed7e4431e7e36c4140df95422150787b1
Jiahao Liu <jiahao.liu@talabat.com>
2022-04-06 17:11:51 +0400

    JL Setting up the final score

--- section2/flutter_complete_guide/lib/main.dart
+++ section2/flutter_complete_guide/lib/main.dart
@@ -14,33 +14,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
   final _questions = const [
     {
       'questionText': "What's pour favorite color?",
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': "What's your favorite animal?",
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9}
+      ],
     },
     {
       'questionText': "What's your favorite instructor?",
-      'answers': ['Max', 'Max', 'Max', 'Max'],
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
 
-  void _answerQuestion() {
-    var aBool = true;
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
-    }
+  var _questionIndex = 0;
+  int _totalScore = 0;
 
+  void _answerQuestion(int score) {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    print(_questionIndex);
-    print('Answer Chosen');
+    print('Question #' + _questionIndex.toString());
+    print('Score: ' + score.toString());
+    _totalScore += score;
+    print('Final score ' + _totalScore.toString());
   }
 
   @override
@@ -56,7 +70,7 @@ class _MyAppState extends State<MyApp> {
                   questionIndex: _questionIndex,
                   answerQuestion: _answerQuestion,
                 )
-              : Result()),
+              : Result(_totalScore)),
     );
   }
 }

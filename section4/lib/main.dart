9ee6652172710b85e0a31fe6a3293b0f0bf7a387
Jiahao Liu <jiahao.liu@talabat.com>
2022-04-15 18:31:48 +0400

    JL Added the basic layout for section 4 for each one of the items

--- section4/lib/main.dart
+++ section4/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,15 +15,58 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactionsList = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New shirt', amount: 98.45, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: 'Water bill', amount: 100.08, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactionsList.map((transaction) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        transaction.amount.toString(),
+                      ),
+                    ),
+                    Column(children: [
+                      Text(transaction.title),
+                      Text(
+                        transaction.date.toString(),
+                      ),
+                    ])
+                  ],
+                ));
+              }).toList(),
+            ),
+          ]),
     );
   }
 }

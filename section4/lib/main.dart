3e10f7486877866b8218fe14a85d7d2484a0db40
Jiahao Liu <jiahao.liu@talabat.com>
2022-04-19 19:17:10 +0400

    JL Added the internationalization package for date formatter. Formattin the date the the currency

--- section4/lib/main.dart
+++ section4/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -37,7 +38,9 @@ class MyHomePage extends StatelessWidget {
               width: double.infinity,
               child: Card(
                 color: Colors.blue,
-                child: Text('CHART!'),
+                child: Text(
+                  'CHART!',
+                ),
                 elevation: 5,
               ),
             ),
@@ -50,16 +53,24 @@ class MyHomePage extends StatelessWidget {
                       margin:
                           EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                       decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black, width: 2)),
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        transaction.amount.toString(),
+                        '\$${transaction.amount}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
                       ),
                     ),
                     Column(children: [
-                      Text(transaction.title),
+                      Text(transaction.title,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
                       Text(
-                        transaction.date.toString(),
+                        DateFormat.yMMMd().format(transaction.date),
                       ),
                     ])
                   ],

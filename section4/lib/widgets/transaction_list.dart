3f15696fdf73fb50129b8a5b4909745bbd9f19e6
Jiahao Liu <jiahao.liu@talabat.com>
2022-04-22 18:50:55 +0400

    JL Added image to the assets and use it on the transactions list

--- section4/lib/widgets/transaction_list.dart
+++ section4/lib/widgets/transaction_list.dart
@@ -11,39 +11,60 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350,
-      child: ListView.builder(
-        itemBuilder: (context, position) {
-          return Card(
-              child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      color: Theme.of(context).primaryColor, width: 2),
+      child: _transactionsList.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$${_transactionsList[position].amount.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+                const SizedBox(
+                  height: 10,
+                ),
+                const Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-              Column(children: [
-                Text(_transactionsList[position].title,
-                    style: Theme.of(context).textTheme.headline6),
-                Text(
-                  DateFormat.yMMMd().format(_transactionsList[position].date),
-                ),
-              ])
-            ],
-          ));
-        },
-        itemCount: _transactionsList.length,
-      ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, position) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).primaryColor, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${_transactionsList[position].amount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Column(children: [
+                      Text(_transactionsList[position].title,
+                          style: Theme.of(context).textTheme.headline6),
+                      Text(
+                        DateFormat.yMMMd()
+                            .format(_transactionsList[position].date),
+                      ),
+                    ])
+                  ],
+                ));
+              },
+              itemCount: _transactionsList.length,
+            ),
     );
   }
 }

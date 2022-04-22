fee2fd15091ec764c016898f908c2cf5d7b78f29
Jiahao Liu <jiahao.liu@talabat.com>
2022-04-22 19:32:54 +0400

    JL Showing partially the chart

--- section4/lib/widgets/chart.dart
+++ section4/lib/widgets/chart.dart
@@ -44,7 +44,9 @@ class Chart extends StatelessWidget {
         elevation: 6,
         margin: EdgeInsets.all(20),
         child: Row(
-          children: [],
+          children: groupedTransactionsValues.map((data) {
+            return Text('${data['day']}: ${data['amount']}');
+          }).toList(),
         ));
   }
 }

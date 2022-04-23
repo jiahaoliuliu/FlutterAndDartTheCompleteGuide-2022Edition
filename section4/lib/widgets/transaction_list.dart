c3e2952f316de84bf15ec9b1b9f98fbac78b088a
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-04-23 17:20:53 +0400

    Set the amount as circle

--- section4/lib/widgets/transaction_list.dart
+++ section4/lib/widgets/transaction_list.dart
@@ -33,35 +33,31 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, position) {
                 return Card(
-                    child: Row(
-                  children: [
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Theme.of(context).primaryColor, width: 2),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${_transactionsList[position].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            '\$${_transactionsList[position].amount}',
+                          ),
                         ),
                       ),
                     ),
-                    Column(children: [
-                      Text(_transactionsList[position].title,
-                          style: Theme.of(context).textTheme.headline6),
-                      Text(
-                        DateFormat.yMMMd()
-                            .format(_transactionsList[position].date),
-                      ),
-                    ])
-                  ],
-                ));
+                    title: Text(_transactionsList[position].title,
+                        style: Theme.of(context).textTheme.headline6),
+                    subtitle: Text(
+                      DateFormat.yMMMd()
+                          .format(_transactionsList[position].date),
+                    ),
+                  ),
+                );
               },
               itemCount: _transactionsList.length,
             ),

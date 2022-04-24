9aa360cc849ae9ac216362cba3b84580fdd3460c
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-04-24 13:45:14 +0400

    Adjusted the heights of the widgets inside of the chart dynamically

--- section4/lib/widgets/chart.dart
+++ section4/lib/widgets/chart.dart
@@ -45,8 +45,6 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionsValues);
-
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),

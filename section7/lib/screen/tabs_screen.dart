eff106d0b83f9e9508f0160a33e2f265669f48f4
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-08-05 15:10:48 +0400

    Updated the style of the selecte/unselected item

--- section7/lib/screen/tabs_screen.dart
+++ section7/lib/screen/tabs_screen.dart
@@ -27,12 +27,18 @@ class _TabsScreenState extends State<TabsScreen> {
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.shifting,
         items: [
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
             icon: Icon(Icons.category),
             label: 'Categories',
           ),
           BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
             icon: Icon(Icons.star),
             label: 'Favorites',
           ),

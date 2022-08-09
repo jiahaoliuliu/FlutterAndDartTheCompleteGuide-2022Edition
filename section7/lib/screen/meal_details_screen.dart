1ae191a16a647dd20c89d98e9cb806d0a7133640
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-08-09 16:38:09 +0400

    Added a floating action button to remove the current screen

--- section7/lib/screen/meal_details_screen.dart
+++ section7/lib/screen/meal_details_screen.dart
@@ -85,6 +85,14 @@ class MeanDetailsScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.delete,
+        ),
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
+      ),
     );
   }
 }

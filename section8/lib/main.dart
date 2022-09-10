a4ca6fe964a6c278b4f876d228270ca4ab711c6d
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-10 19:22:26 +0200

    Section8: Created the product provider. Passing the list of products from main to product overview screen to the product details screen

--- section8/lib/main.dart
+++ section8/lib/main.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/product_overview_screen.dart';
 import './screens/product_details_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange, // Deprecated
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      // Version 3.x of provider
+      builder: (ctx) => Products(),
+      // Version 4.x+ of provider
+      // create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange, // Deprecated
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverViewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+        },
       ),
-      home: ProductOverViewScreen(),
-      routes: {
-        ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
-      },
     );
   }
 }
--- section8/lib/screens/product_details_screen.dart
+++ section8/lib/screens/product_details_screen.dart
@@ -13,6 +13,9 @@ class ProductDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    // ..
+
     return Scaffold(
       appBar: AppBar(
         title: Text('title'),

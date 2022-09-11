afd316aa7ff14344ccc96f5cbd8904a3e476fd0c
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-11 20:32:18 +0200

    Section8: Using the provider to get the right product

--- section8/lib/screens/product_details_screen.dart
+++ section8/lib/screens/product_details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
+
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
@@ -11,10 +14,13 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context)
+        .items
+        .firstWhere((product) => product.id == productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }

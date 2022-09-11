51c904e75a3bd5ad7f6393b42e4b3f162f358c47
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-11 18:06:34 +0200

    Section8: Set up the web view. Added the normal item with Grid view

--- section8/lib/main.dart
+++ section8/lib/main.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import './screens/product_overview_screen.dart';
-import './screens/product_details_screen.dart';
-import './providers/products.dart';
+import 'package:flutter_complete_guide/screens/product_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      // Version 3.x of provider
-      builder: (ctx) => Products(),
-      // Version 4.x+ of provider
-      // create: (ctx) => Products(),
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange, // Deprecated
-          fontFamily: 'Lato',
-        ),
-        home: ProductOverViewScreen(),
-        routes: {
-          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
-        },
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
+      home: ProductOverviewScreen(),
     );
   }
 }
--- section8/lib/widgets/product_item.dart
+++ section8/lib/widgets/product_item.dart
@@ -1,51 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../providers/product.dart';
-
-import '../screens/product_details_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  // final String id;
-  // final String title;
-  // final String imageUrl;
+  final String id;
+  final String title;
+  final String imageUrl;
 
-  // ProductItem(this.id, this.title, this.imageUrl);
+  ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailsScreen.routeName,
-                arguments: product.id);
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            icon: Icon(Icons.favorite),
             onPressed: () {},
             color: Theme.of(context).accentColor,
           ),
           title: Text(
-            product.title,
+            title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
             icon: Icon(
               Icons.shopping_cart,
             ),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
+            onPressed: () {},
             color: Theme.of(context).accentColor,
           ),
         ),

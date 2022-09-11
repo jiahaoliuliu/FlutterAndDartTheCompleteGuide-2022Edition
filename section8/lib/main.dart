2918abb0bfaa2a58c162d3a721b065d7fbc7fcdf
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-11 18:44:50 +0200

    Section8: Upgraded the version of the provider till 4.0. It solves a small problem

--- section8/lib/main.dart
+++ section8/lib/main.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '/screens/product_overview_screen.dart';
 import 'screens/product_details_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      // Provider <= 3: Use builder
+      // builder: (ctx) => Products(),
+      // Provider >= 4: Use create
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (context) => ProductDetailsScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailsScreen.routeName: (context) => ProductDetailsScreen(),
-      },
     );
   }
 }

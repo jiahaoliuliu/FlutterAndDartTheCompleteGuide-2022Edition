4067c68766bb68bcab1cdfcff6c633ec35724804
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-11 23:21:42 +0200

    Section8: Setting favorite as part of the single screen and not a general property

--- section8/lib/screens/product_overview_screen.dart
+++ section8/lib/screens/product_overview_screen.dart
@@ -2,14 +2,49 @@ import 'package:flutter/material.dart';
 
 import '../widgets/product_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
+  @override
+  State<ProductOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My shop'),
-      ),
-      body: ProductGrid(),
+      appBar: AppBar(title: Text('My shop'), actions: <Widget>[
+        PopupMenuButton(
+          onSelected: (FilterOptions selectedValue) {
+            setState(() {
+              if (selectedValue == FilterOptions.Favorites) {
+                _showOnlyFavorites = true;
+              } else {
+                _showOnlyFavorites = false;
+              }
+            });
+          },
+          icon: Icon(
+            Icons.more_vert,
+          ),
+          itemBuilder: (_) => [
+            PopupMenuItem(
+              child: Text('Only Favorites'),
+              value: FilterOptions.Favorites,
+            ),
+            PopupMenuItem(
+              child: Text('Show All'),
+              value: FilterOptions.All,
+            )
+          ],
+        ),
+      ]),
+      body: ProductGrid(_showOnlyFavorites),
     );
   }
 }

e99750e58914ef28c5e3f29e845eeb592299ab60
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-13 22:48:31 +0200

    Section8: implemented the order button

--- section8/lib/screens/cart_screen.dart
+++ section8/lib/screens/cart_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart.dart' show Cart;
 import '../widgets/cart_item.dart';
-import '../providers/Orders.dart';
+import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';

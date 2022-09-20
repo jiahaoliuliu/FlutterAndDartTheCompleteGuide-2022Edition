b865907455b5137e9fe258054a103c84c588512e
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-20 17:51:58 +0200

    Section9: Set the proper item builder for the product. Added a simple drawer to the screen

--- section8/lib/main.dart
+++ section8/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 
 import '/screens/product_overview_screen.dart';
 import '/screens/product_details_screen.dart';
-import './screens/orders_screen.dart';
 import './screens/user_product_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';

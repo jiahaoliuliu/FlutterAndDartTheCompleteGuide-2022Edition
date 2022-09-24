4cce1c9a219333ee81fd6813e016e8c4112a3128
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-24 19:06:42 +0200

    Section10: Posting the product to the firebase server once it is created. Using the id generated on the firebase as the id of the product

--- section8/lib/providers/products.dart
+++ section8/lib/providers/products.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 import 'product.dart';
 
 class Products with ChangeNotifier {
@@ -65,14 +68,33 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    final newProduct = Product(
+    const url =
+        'https://flutter-course-on-udemy-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+    http
+        .post(
+      url,
+      body: json.encode(
+        {
+          'title': product.title,
+          'description': product.description,
+          'imageUrl': product.imageUrl,
+          'price': product.price,
+          'isFavorite': product.isFavorite,
+        },
+      ),
+    )
+        .then((response) {
+      print(json.decode(response.body));
+      final newProduct = Product(
         title: product.title,
         description: product.description,
         price: product.price,
         imageUrl: product.imageUrl,
-        id: DateTime.now().toString());
-    _items.add(newProduct);
-    notifyListeners();
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {

481dbaaab0a3bf2e0d20347f275c9e9d9b67257d
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-25 21:29:52 +0200

    Section10: Updated orders dart to post an order when a new order is added

--- section8/lib/providers/Orders.dart
+++ section8/lib/providers/Orders.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-import './cart.dart';
+import '../widgets/cart_item.dart';
 
 class OrderItem {
   final String id;
@@ -23,14 +25,34 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url =
+        'https://flutter-course-on-udemy-default-rtdb.europe-west1.firebasedatabase.app/orders/$id.json';
+    final timestamp = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        },
+      ),
+    );
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
         products: cartProducts,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
       ),
     );
     notifyListeners();

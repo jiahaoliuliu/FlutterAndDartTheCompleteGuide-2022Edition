5fda050472b2acb9d641fad0cd11868bfd5f235c
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-25 22:21:54 +0200

    Section10: Update the UI to sending the orders to the server

--- section8/lib/screens/cart_screen.dart
+++ section8/lib/screens/cart_screen.dart
@@ -40,20 +40,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  ElevatedButton(
-                    child: Text('Order now'),
-                    onPressed: () {
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      cart.clear();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      textStyle:
-                          TextStyle(color: Theme.of(context).primaryColor),
-                    ),
-                  ),
+                  OrderButton(cart: cart),
                 ],
               ),
             ),
@@ -76,5 +63,52 @@ class CartScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  final Cart cart;
+
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Text('Order now'),
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+              setState(() {
+                _isLoading = true;
+              });
+
+              await Provider.of<Orders>(context, listen: false).addOrder(
+                widget.cart.items.values.toList(),
+                widget.cart.totalAmount,
+              );
+              widget.cart.clear();
+              setState(() {
+                _isLoading = false;
+              });
+            },
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(color: Theme.of(context).primaryColor),
+      ),
+    );
+    ;
   }
 }

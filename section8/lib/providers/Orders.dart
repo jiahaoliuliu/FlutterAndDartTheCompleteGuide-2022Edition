3fe7bb0a36af98628dd4e89c35e39f5970eb5dec
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-25 23:42:03 +0200

    Section10: Adding the orders and fetching the orders in a correct order

--- section8/lib/providers/Orders.dart
+++ section8/lib/providers/Orders.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 import './cart.dart';
 
@@ -29,7 +30,33 @@ class Orders with ChangeNotifier {
     const url =
         'https://flutter-course-on-udemy-default-rtdb.europe-west1.firebasedatabase.app/orders.json';
     final response = await http.get(url);
-    print(json.decode(response.body));
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+            id: orderId,
+            amount: orderData['amount'],
+            dateTime: DateTime.parse(
+              orderData['dateTime'],
+            ),
+            products: (orderData['products'] as List<dynamic>)
+                .map(
+                  (item) => CartItem(
+                    id: item['id'],
+                    price: item['price'],
+                    quantity: item['quantity'],
+                    title: item['title'],
+                  ),
+                )
+                .toList()),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {

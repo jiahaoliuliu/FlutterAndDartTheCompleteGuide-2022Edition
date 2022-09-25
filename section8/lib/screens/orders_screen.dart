dc840dc5c86c60626f404457aee399f8b0f5fb11
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-25 23:45:19 +0200

    Section10: Improved the way how the status is handled on OrderScreen

--- section8/lib/screens/orders_screen.dart
+++ section8/lib/screens/orders_screen.dart
@@ -17,15 +17,13 @@ class _OrdersScreenState extends State<OrdersScreen> {
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+    _isLoading = true;
+    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
       setState(() {
         _isLoading = false;
       });
     });
+    // });
     super.initState();
   }
 

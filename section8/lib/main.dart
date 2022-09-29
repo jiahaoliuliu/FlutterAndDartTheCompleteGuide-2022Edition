2d07671d210aced6552b3b784ad94dbbc8ac403b
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-09-29 21:47:38 +0200

    Section11: Added a simple auth screen

--- section8/lib/main.dart
+++ section8/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 import '/screens/product_overview_screen.dart';
 import '/screens/product_details_screen.dart';
 import './screens/user_product_screen.dart';
+import './screens/auth_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailsScreen.routeName: (context) => ProductDetailsScreen(),
           CartScreen.routeName: (context) => CartScreen(),

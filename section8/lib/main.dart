6f7c447c70ef945a3ec763015a80fcd7744ff027
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-10-09 15:32:39 +0200

    Section11: Created the method to sign people up

--- section8/lib/main.dart
+++ section8/lib/main.dart
@@ -8,6 +8,7 @@ import '/screens/product_overview_screen.dart';
 import '/screens/product_details_screen.dart';
 import './screens/user_product_screen.dart';
 import './screens/auth_screen.dart';
+import './providers/auth.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
---
+++ section8/lib/providers/auth.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> signup(String email, String password) async {
+    const url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxzsb2ogQFW4gOuA0ZG1QfRM7cxByfypA";
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(
+      json.decode(response.body),
+    );
+  }
+}

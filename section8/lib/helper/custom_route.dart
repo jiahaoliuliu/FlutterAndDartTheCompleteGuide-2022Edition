4d4d6c3a2af7451b5871093d4844588e1a948baa
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-10-21 19:06:40 +0200

    Section12: Added the custom transition

--- section8/lib/helper/custom_route.dart
+++ section8/lib/helper/custom_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// For single routes
 class CustomRoute<T> extends MaterialPageRoute<T> {
   CustomRoute({WidgetBuilder builder, RouteSettings settings})
       : super(
@@ -16,6 +17,27 @@ class CustomRoute<T> extends MaterialPageRoute<T> {
   ) {
     // if the first screen, do not animate
     if (settings.name == "/") {
+      return child;
+    }
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
+
+// For all the routes
+class CustomPageTransitionBuilder extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    // if the first screen, do not animate
+    if (route.settings.name == "/") {
       return child;
     }
     return FadeTransition(

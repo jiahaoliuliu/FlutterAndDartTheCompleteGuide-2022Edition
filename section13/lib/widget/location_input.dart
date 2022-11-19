3278a7fc61f6f4e9a7af65cd1e60767446dcc34a
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-19 16:53:32 +0400

    Section13: Allow the user to see google maps with default point to Googleplex

--- section13/lib/widget/location_input.dart
+++ section13/lib/widget/location_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput();
@@ -20,6 +21,21 @@ class _LocationInputState extends State<LocationInput> {
     setState(() {
       _previewImageUrl = staticMapImageUrl;
     });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+
+    if (selectedLocation == null) {
+      return;
+    }
+    // else
   }
 
   @override
@@ -55,7 +71,7 @@ class _LocationInputState extends State<LocationInput> {
                   foregroundColor: Theme.of(context).primaryColor),
             ),
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _selectOnMap,
               icon: Icon(Icons.map),
               label: Text('Select on Map'),
               style: TextButton.styleFrom(

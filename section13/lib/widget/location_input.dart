0a63b97ffab43b95aa788f253b0ea4972423e846
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-19 16:17:08 +0400

    Section13: Allow the app to get the location of the user and display it on the screen

--- section13/lib/widget/location_input.dart
+++ section13/lib/widget/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import '../helpers/location_helper.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput();
@@ -13,8 +14,12 @@ class _LocationInputState extends State<LocationInput> {
 
   Future<void> _getCurrentUserLocation() async {
     final locData = await Location().getLocation();
-    print(locData.latitude);
-    print(locData.longitude);
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: locData.latitude, longitude: locData.longitude);
+
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
   }
 
   @override

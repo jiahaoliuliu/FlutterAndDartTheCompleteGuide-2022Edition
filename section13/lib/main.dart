243bf0e26e7b8faa3648338107f76808a2d6d97e
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-24 15:52:47 +0400

    Section13: Created the details screen for the place

--- section13/lib/main.dart
+++ section13/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/place_details_screen.dart';
 import 'package:provider/provider.dart';
 
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
+import './screens/place_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,6 +23,7 @@ class MyApp extends StatelessWidget {
         home: PlacesListScreen(),
         routes: {
           AddPlaceScreen.routename: (ctx) => AddPlaceScreen(),
+          PlaceDetailscreen.routeName: (ctx) => PlaceDetailscreen(),
         },
       ),
     );

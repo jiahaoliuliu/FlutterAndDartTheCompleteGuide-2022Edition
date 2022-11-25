4629917c2d18964b1374aff35060f54b23605143
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-25 17:58:00 +0400

    Section14: Created a basic stream builder that receives the data from the firebase server as stream and display it as list in the screen

--- section14/lib/screens/chat_screen.dart
+++ section14/lib/screens/chat_screen.dart
@@ -5,26 +5,28 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works!'),
-        ),
-      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chats/j0xvCssGlYSoyAWwBtFu/messages')
+              .snapshots(),
+          builder: (ctx, streamSnapshot) {
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = streamSnapshot.data.docs;
+            return ListView.builder(
+              itemCount: streamSnapshot.data.docs.length,
+              itemBuilder: (ctx, index) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text(documents[index]['text']),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/j0xvCssGlYSoyAWwBtFu/messages')
-              .snapshots()
-              .listen((data) {
-            // print(data.docs[0]['text']);
-            data.docs.forEach((document) {
-              print(document['text']);
-            });
-          });
-        },
+        onPressed: () {},
       ),
     );
   }

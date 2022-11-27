f439857a477fe24422fa590d894c0e08c4b98da2
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-27 22:19:28 +0100

    Section14: Adding the image url as part of the message

--- section14/lib/widgets/chat/message_bubble.dart
+++ section14/lib/widgets/chat/message_bubble.dart
@@ -6,55 +6,79 @@ class MessageBubble extends StatelessWidget {
   final bool isMe;
   final Key key;
   final String username;
-  MessageBubble(this.message, this.username, this.isMe, {this.key});
+  final String userImage;
+  MessageBubble(this.message, this.username, this.userImage, this.isMe,
+      {this.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
-                bottomRight: isMe ? Radius.circular(0) : Radius.circular(12)),
-          ),
-          width: 140,
-          padding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          margin: EdgeInsets.symmetric(
-            vertical: 4,
-            horizontal: 8,
-          ),
-          child: Column(
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            children: [
-              Text(
-                username,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: isMe
-                        ? Colors.black
-                        : Theme.of(context).accentTextTheme.titleMedium.color),
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                    bottomLeft:
+                        !isMe ? Radius.circular(0) : Radius.circular(12),
+                    bottomRight:
+                        isMe ? Radius.circular(0) : Radius.circular(12)),
               ),
-              Text(
-                message,
-                style: TextStyle(
-                    color: isMe
-                        ? Colors.black
-                        : Theme.of(context).accentTextTheme.titleMedium.color),
-                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              width: 140,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
               ),
-            ],
+              margin: EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 8,
+              ),
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    username,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: isMe
+                            ? Colors.black
+                            : Theme.of(context)
+                                .accentTextTheme
+                                .titleMedium
+                                .color),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                        color: isMe
+                            ? Colors.black
+                            : Theme.of(context)
+                                .accentTextTheme
+                                .titleMedium
+                                .color),
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: -10,
+          left: isMe ? null : 130,
+          right: isMe ? 130 : null,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(userImage),
           ),
         ),
       ],
+      clipBehavior: Clip.none,
     );
   }
 }
--- section14/lib/widgets/chat/messages.dart
+++ section14/lib/widgets/chat/messages.dart
@@ -24,6 +24,7 @@ class Messages extends StatelessWidget {
           itemBuilder: (ctx, index) => MessageBubble(
             chatDocs[index]['text'],
             chatDocs[index]['username'],
+            chatDocs[index]['userImage'],
             chatDocs[index]['userId'] == FirebaseAuth.instance.currentUser.uid,
             key: ValueKey(chatDocs[index].id),
           ),

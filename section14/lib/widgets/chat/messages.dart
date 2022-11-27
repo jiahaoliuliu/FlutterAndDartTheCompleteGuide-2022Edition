899e3705ed9b26118358a8e01fffc174b6d50f84
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-27 13:49:47 +0100

    Section14: Set up the style for the text bubbles

--- section14/lib/widgets/chat/messages.dart
+++ section14/lib/widgets/chat/messages.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_complete_guide/widgets/chat/message_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
@@ -19,7 +20,7 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) => Text(
+          itemBuilder: (ctx, index) => MessageBubble(
             chatDocs[index]['text'],
           ),
         );

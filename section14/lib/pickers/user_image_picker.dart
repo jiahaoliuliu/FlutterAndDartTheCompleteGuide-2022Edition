5da43db9e82d212d44638755197774438ee6a31c
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-27 20:23:28 +0100

    Section14: Created a simple image picker and made the image compulsore if the user want to sign up

--- section14/lib/pickers/user_image_picker.dart
+++ section14/lib/pickers/user_image_picker.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+
+  final void Function(File pickedImage) imagePickFn;
+
   @override
   State<UserImagePicker> createState() => _UserImagePickerState();
 }
@@ -17,6 +21,7 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImage = File(pickedImageFile.path);
     });
+    widget.imagePickFn(_pickedImage);
   }
 
   @override

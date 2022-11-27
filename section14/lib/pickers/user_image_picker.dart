87b9b40d594d43eef184b4b2429554e9c51b55a8
Jiahao Liu <jiahaoliuliu@gmail.com>
2022-11-27 20:10:11 +0100

    Section14: Allow the user to set image profile. Do not show it up on the login screen

---
+++ section14/lib/pickers/user_image_picker.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+
+  void _pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final pickedImageFile = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _pickedImage = File(pickedImageFile.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+        ),
+      ],
+    );
+  }
+}
